fde33a1a8d57d072061aacc32c4adae4d93bf120
jon.catlin <jonc@destini.com>
2018-11-25 09:41:37 -0700

    Commented out the closure statement to see if runtime error goes away

--- vars/CSLDevelopmentPipeline.groovy
+++ vars/CSLDevelopmentPipeline.groovy
@@ -1,7 +1,7 @@
 sdef call(body) {
     // evaluate the body block, and collect configuration into the object
     def config = [:]
-    body.resolveStrategy = Closure.DELEGATE_FIRST
+//    body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
     body()
 
